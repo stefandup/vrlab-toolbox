3591859c08570875ebdb9dbe3ba27e025608befd
stefan <stefandup@sun.ac.za>
2026-04-24 17:25:35 +0200

    Fixed crane batch script install

---
+++ src/mooi_toolbox/cli/vrlab_crane_process_batch.py
@@ -0,0 +1,72 @@
+import click
+import logging
+import os
+from pathlib import Path
+import pandas as pd
+
+from mooi_toolbox import mobi_logging
+from mooi_toolbox.processing import biopac
+from mooi_toolbox.processing.crane_pipeline import run_pipeline as run_crane_pipeline
+from mooi_toolbox.processing.plot_utils import save_plot
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.argument("input_folder", type=click.Path(exists=True, dir_okay=True), required=True)
+@click.argument("output_folder", type=click.Path(exists=True, dir_okay=True), required=False)
+@click.option("--verbose", is_flag=True, help="Give verbose output")
+def main(input_folder: str, output_folder: str, verbose: bool):
+    """CLI tool for batch processing VRLab crane behaviour and physiology data."""
+    if not output_folder:
+        output_folder = input_folder + "_out"
+
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    logger.info("Looking into input folder: %s. Output folder: %s", input_folder, output_folder)
+
+    out_fn = os.path.join(output_folder, "vrlab_crane_process_batch_out.csv")
+
+    root = Path(input_folder)
+    out_file_parts = []
+
+    for biopac_mat_fn in root.rglob("*.mat"):
+        subject_id = biopac.get_subject_id_from_mat(biopac_mat_fn)
+
+        mobi_logging.log_section(logger, f"Subject {subject_id}")
+        try:
+            participant_data_out, fig = run_crane_pipeline(
+                biopac_mat_fn,
+                verbose=verbose,
+                show_plots=False,
+            )
+
+            try:
+                save_plot(fig, output_folder, subject_id, f"Subject {subject_id} QC")
+            except AttributeError as error:
+                logger.info("Error saving plot.%s", error)
+
+            if participant_data_out.empty:
+                logger.warning("No participant output for subject %s", subject_id)
+                continue
+
+            participant_data_out = participant_data_out.reset_index(drop=True)
+            participant_data_out.insert(0, "Subject_ID", subject_id)
+
+            out_file_parts.append(participant_data_out)
+            logger.info("Done crane pipeline for subject %s", subject_id)
+
+        except (FileNotFoundError, ValueError, KeyError, TypeError) as error:
+            logger.warning("Skipping subject %s because processing failed: %s", subject_id, error)
+            continue
+
+    out_df = pd.concat(out_file_parts, axis=0)
+    out_df.to_csv(out_fn)
+
+    logger.info("Saved output to %s", out_fn)
+
+
+if __name__ == "__main__":
+    mobi_logging.init(__file__)
+    main()
