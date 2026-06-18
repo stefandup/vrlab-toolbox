de1a05c20f862b0ca0295068b694ca33ef89f386
stefan <stefandup@sun.ac.za>
2026-06-18 09:35:16 +0200

    Added trigger times predictor.

--- src/mooi_toolbox/processing/vr_intervals.py
+++ src/mooi_toolbox/processing/vr_intervals.py
@@ -1,5 +1,8 @@
 import logging
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 from mooi_toolbox.read_mobi_xdf import xdf_io
 from mooi_toolbox import config as cfg
 from mooi_toolbox.processing.processing_status import ProcessingStatus
@@ -117,12 +120,26 @@ def get_crane_behav_intervals(validated_behav_df : pd.DataFrame) -> dict[str,tup
 
     return intervals_out
 
+def get_predicted_trigger_times(behav_intervals : dict[str, tuple[float, float]]) -> np.ndarray:
+
+    reference_df = pd.read_parquet(r"C:\\Users\\stefan\\mobi_mooi_toolbox\\matched_debug_df_testa.parquet")
+
+    # Control for the relative start difference.
+    X = (reference_df["behav_start"].loc[1:len(behav_intervals)] - reference_df["behav_start"].iloc[0]).values.reshape(-1,1)
+    y = (reference_df["trigger_start"].loc[1:len(behav_intervals)] - reference_df["trigger_start"].iloc[0]).values.reshape(-1,1)
+    model = LinearRegression()
+    model.fit(X,y)
+    newX = np.array([start_end_time[0] for start_end_time in behav_intervals.values()]).reshape(-1,1)
+    newX_rel = newX - newX[0]
+
+    return model.predict(newX_rel)
 
 def match_behav_intervals_with_trigger_intervals(trigger_intervals : dict[str,tuple[float,float]],validated_behav_df) -> tuple[dict[str,tuple[float,float]],ProcessingStatus]:
     #TODO: Make more robust
-    status = ProcessingStatus.OK
     behav_intervals = get_crane_behav_intervals(validated_behav_df)
-    #_behav_intervals = behav_intervals.copy()
+    predicted_trigger_times = get_predicted_trigger_times(behav_intervals)
+    status = ProcessingStatus.OK
+    
     remaining_trigger_intervals = trigger_intervals.copy()
     matched_intervals = {}
     unmatched_behav_keys = set(behav_intervals.keys())
@@ -130,6 +147,7 @@ def match_behav_intervals_with_trigger_intervals(trigger_intervals : dict[str,tu
     best_deltas = []
     best_signed_deltas = []
     matched_debug_rows = []
+
     for behav_key, behav_interval in behav_intervals.items():
 
         behav_start = behav_interval[0]
@@ -144,7 +162,7 @@ def match_behav_intervals_with_trigger_intervals(trigger_intervals : dict[str,tu
             
             delta = abs(behav_start - trigger_start)
             signed_delta = behav_start - trigger_start
-            #print(f"Trigger {trigger_key} : Time {trigger_interval[0]} :  Delta: {delta}")
+            print(f"Trigger {trigger_key} : Time {trigger_interval[0]} :  Delta: {delta}")
             if delta < best_delta:
                 best_trigger_key = trigger_key
                 best_delta = delta
@@ -164,30 +182,43 @@ def match_behav_intervals_with_trigger_intervals(trigger_intervals : dict[str,tu
                 "behav_key": behav_key,
                 "trigger_key": best_trigger_key,
                 "behav_start": behav_start,
+                "behav_end" : behav_interval[1],
                 "trigger_start": matched_intervals[behav_key][0],
-                "signed_delta": behav_start - matched_intervals[behav_key][0],
+                "trigger_end" : matched_intervals[behav_key][1],
+                "behav_dur" : behav_interval[1] - behav_interval[0],
+                "trigger_dur" : matched_intervals[behav_key][1] - matched_intervals[behav_key][0],
+                "dur_diff" : (matched_intervals[behav_key][1] - matched_intervals[behav_key][0]) - (behav_interval[1] - behav_interval[0]),
+                "trigger_behav_start_diff": behav_start - matched_intervals[behav_key][0],
             }
         )
-
-
         unmatched_behav_keys.remove(behav_key)
 
         print(f"Selected: {best_trigger_key} with best delta: {best_delta} as best match for {behav_key}")
         best_deltas.append(best_delta)
         best_signed_deltas.append(best_signed_delta)
+    matched_debug_df = pd.DataFrame(matched_debug_rows)
+    matched_debug_df.to_parquet("matched_debug_df_20.parquet")
     print(f"Best deltas: {best_deltas} Signed: {best_signed_deltas}")
-
+    # TODO: Out df for debugging
+    debug_rows = []
     for previous_row, current_row in zip(matched_debug_rows[:-1], matched_debug_rows[1:]):
         behav_spacing = current_row["behav_start"] - previous_row["behav_start"]
         trigger_spacing = current_row["trigger_start"] - previous_row["trigger_start"]
         spacing_error = behav_spacing - trigger_spacing
-
+        debug_rows.append({"First_Block_behav" : current_row['behav_key'],
+                           "Previous_Block_behav" : previous_row['behav_key'],
+                           "Behav_spacing" : behav_spacing,
+                           "Trigger_spacing" : trigger_spacing,
+                           "Spacing_Error" : spacing_error
+                           })
         print(
             f"{previous_row['behav_key']} -> {current_row['behav_key']}: "
             f"behav_spacing={behav_spacing:.3f}, "
             f"trigger_spacing={trigger_spacing:.3f}, "
             f"spacing_error={spacing_error:.3f}"
         )
+    debug_df = pd.DataFrame(debug_rows)
+    debug_df.to_parquet("vr_debug_out.parquet", index=False)
 
     if len(unmatched_behav_keys) != 0:
         logger.warning("Could not match %s",unmatched_behav_keys)
