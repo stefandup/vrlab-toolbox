d7ba6c7672cb5177d3bf46f49120146e7568da9c
stefan <stefandup@sun.ac.za>
2026-07-02 08:53:36 +0200

    Added append_column method to PipelinData

--- src/mooi_toolbox/processing/output_data.py
+++ src/mooi_toolbox/processing/output_data.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from matplotlib.figure import Figure
 import pandas as pd
 import pandera.pandas as pa
 
 from mooi_toolbox.processing.processing_status import PipelineStatus
-from mooi_toolbox.processing.input_data import ParticipantConfig
 
 def build_base_output_schema( additional_columns : dict[str, pa.Column] | None = None) -> pa.DataFrameSchema:
     '''Ensure that all pipelines have Subject_ID and Processing_Status values'''
@@ -20,24 +19,55 @@ def build_base_output_schema( additional_columns : dict[str, pa.Column] | None =
 
 @dataclass
 class PipelineData():
-    subject_df_out : pd.DataFrame
-    figure_data_out : Figure | None
-    status : PipelineStatus
+    #TODO: Fix that on init it inits already an empty participant output data using config.
+    subject_id : str
+    subject_df_out : pd.DataFrame = field(init=False)
+    figure_data_out : Figure | None = field(default=None, init=False)
+    status : PipelineStatus = field(default_factory=PipelineStatus, init=False)
 
     def __post_init__(self):
+        self.subject_df_out = pd.DataFrame([{
+             "Subject_ID" : self.subject_id, 
+             "Processing_Status" : self.status.get_as_text()
+             }])
+        
         self.subject_df_out = self.validate_participant_output()
 
     def validate_participant_output(self) -> pd.DataFrame:
         return build_base_output_schema().validate(self.subject_df_out)
 
-    @classmethod
-    def error(cls, data_in : ParticipantConfig , status_in : PipelineStatus):
-        error_df_out = pd.DataFrame(
-            {"Subject_ID" : [data_in.subject_id],
-             "Processing_Status" : [status_in.get_as_text()]})
+    def append_columns(self, data_in : pd.DataFrame, additional_columns : dict[str, pa.Column]) -> None:
+        revised_validation_schema = build_base_output_schema(additional_columns)
+        data_in_reset = data_in.reset_index(drop=True)
+
+        # Drop columns from data in.
         
-        return cls(
-            subject_df_out = error_df_out,
-            figure_data_out = None,
-            status=status_in
+        data_in_reset = data_in_reset.drop(
+                columns=["Subject_ID", "Processing_Status"],
+                errors="ignore"
             )
+
+        existing_df = self.subject_df_out
+        existing_df_reset = existing_df.reset_index(drop=True)
+        data_list = [existing_df_reset,data_in_reset]
+        combined_df = pd.concat(data_list, axis=1)
+
+        if "Subject_ID" not in combined_df.columns:
+                combined_df.insert(0, "Subject_ID", self.subject_id)
+
+        if not len(combined_df) == 1:
+            raise ValueError("Output data must contain exactly one row.")
+
+        self.subject_df_out = revised_validation_schema.validate(combined_df)
+
+    @classmethod
+    def error(cls,subject_id : str ,status_in : PipelineStatus) -> "PipelineData":
+        error_df_out = pd.DataFrame(
+            {"Subject_ID" : [subject_id],
+             "Processing_Status" : [status_in.get_as_text()]})
+        result = cls(subject_id)
+        result.subject_df_out = error_df_out
+        result.figure_data_out = None
+        result.status = status_in
+
+        return result
