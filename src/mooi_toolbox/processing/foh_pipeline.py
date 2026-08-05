267242e736aedcab07e0fa45a88757df9d839553
stefan <stefandup@sun.ac.za>
2026-08-05 15:34:41 +0200

    Minor fix

--- src/mooi_toolbox/processing/foh_pipeline.py
+++ src/mooi_toolbox/processing/foh_pipeline.py
@@ -21,7 +21,10 @@ def has_missing_requirements(missing: set, required: list[str]) -> bool:
 def run_lsl_pipeline(
     xdf_fn: str, verbose: bool, show_plots: bool
 ) -> tuple[pd.DataFrame, Figure | None]:
-    """Run FOH pipeline for LSL EDA, ECG and Behavioural (i.e. Target) data. Tries to be robust wrt missing data."""
+    """
+    Run FOH pipeline for LSL EDA, ECG and Behavioural (i.e. Target) data.
+    Tries to be robust wrt missing data.
+    """
     if not xdf_fn:
         streams = get_and_check_xdf(cfg.get_default_xdf(), verbose=verbose)
     else:
